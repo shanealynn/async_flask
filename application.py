2dab17d1b7bb3050a68e29fe8a48a501336ac5ac
Michael O'Connor <moconnor@medallia.com>
2019-11-20 12:52:49 -0800

    Got rid of uneccessary import

--- application.py
+++ application.py
@@ -24,8 +24,6 @@ from flask import Flask, render_template, url_for, copy_current_request_context
 from random import random
 from time import sleep
 from threading import Thread, Event
-import eventlet
-eventlet.monkey_patch(socket=True)
 
 __author__ = 'slynn'
 
